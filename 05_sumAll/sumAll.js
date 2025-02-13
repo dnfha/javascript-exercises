6fc8340f433c2de29313b5908d9bd39f9d0f2494
dnfha <31221487+dnfha@users.noreply.github.com>
2025-02-14 00:02:53 +0300

    solve:05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (!(Number.isInteger(num1) && Number.isInteger(num2) && num1 + num2 >= 2)) {
+    return "ERROR";
+  }
+  if (num1 + num2 === 2) {
+    return 1;
+  }
+  let sum = 0;
+  let bigNum;
+  let smallNum;
+  if (num1 > num2) {
+    bigNum = num1;
+    smallNum = num2;
+  } else {
+    smallNum = num1;
+    bigNum = num2;
+  }
+  for (let i = smallNum; i <= bigNum; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
