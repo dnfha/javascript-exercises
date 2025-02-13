d9ba116cabdc0f6e2322db47116e2f80814afa25
dnfha <31221487+dnfha@users.noreply.github.com>
2025-02-13 23:08:50 +0300

    solve:03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (aString) {
+    let newString = '';
+    for (let i = aString.length - 1; i >= 0; i--) {
+        newString += aString[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
